692ecc1770b48558ece7b56e1d5daa9a4a451cdd
Will Boafo <boafowill@gmail.com>
2022-04-11 12:06:37 -0400

    .js to .jsx

--- src/components/Suggestions.jsx
+++ src/components/Suggestions.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
+import React from 'react';
 
-const Suggestions = (props) => {
+export const Suggestions = (props) => {
     const options = props.results.map(r => (
         // <li key={r.mal_id}>
         //     {r.title}
         // </li>
-        <div style = {cardStyle}>
-            
-            <img src={r.image_url} alt={r.title} style={imgStyle}/>
-            <p>{r.title}</p>
+        <div style = { cardStyle }>
+        <img src = { r.image_url } alt={ r.title } style={ imgStyle }/>
+        <p> { r.title } </p> 
         </div>
-    ))
-    return <div style = {cards}>{options}</div>
-}
+    ));
+    return (<div style = { cards } > { options } </div>);
+};
 
 const cardStyle = {
     boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)',
@@ -22,14 +21,14 @@ const cardStyle = {
     float: 'left',
     margin: 'auto',
     textAlign: 'center'
-}
+};
 
 const cards = {
     display: 'grid',
     gridTemplateColumns: 'repeat(3, 1fr)',
     gridAutoRows: 'auto',
     gridGap: '1rem'
-}
+};
 
 const imgStyle = {
     display: 'block',
@@ -37,6 +36,6 @@ const imgStyle = {
     marginRight: 'auto',
     width: '80%',
     padding: '5px',
-}
+};
 
-export default Suggestions
+export default Suggestions;
